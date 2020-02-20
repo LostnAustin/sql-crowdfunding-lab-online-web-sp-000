f38019153c12066455cbb8d8c78cc1a1890e7eb8
Chad Lengefeld <chadlengefeld@gmail.com>
2020-02-20 04:22:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,9 +43,10 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount FROM users
+  INNER JOIN pledges ON pledges.project_id = projects.id
   WHERE category = 'music'
 
-  INNER JOIN pledges ON pledges.project_id = projects.id
+
 
   ;"
 end
